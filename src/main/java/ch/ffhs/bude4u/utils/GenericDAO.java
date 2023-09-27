b8e70a7db4ac462a78903bf47b7951f01af23d04
Achim Grether <a.grether@hotmail.com>
2023-09-27 22:33:35 +0200

    remove public initializer in dao interface

--- src/main/java/ch/ffhs/bude4u/utils/GenericDAO.java
+++ src/main/java/ch/ffhs/bude4u/utils/GenericDAO.java
@@ -10,31 +10,31 @@ public interface GenericDAO<T> {
      * @param id the id to filter for
      * @return matching entity
      */
-    public Optional<T> get(String id);
+    Optional<T> get(String id);
 
     /**
      * fetches list of all entities
      * @return list of Type <T>
-     * @TODO: Maybe add pagination?!
+     * {@code @TODO:} Maybe add pagination?!
      */
-    public List<T> getAll();
+    List<T> getAll();
 
     /**
      * creates new entity in database
      * @param t entity to create
      */
-    public void create(T t);
+    void create(T t);
 
     /**
      * update provided entity in database
      * @param t entity to update
      */
-    public void update(T t);
+    void update(T t);
 
     /**
      * delete provided entity in database
      * @param t entity to delete
      */
-    public void delete(T t);
+    void delete(T t);
 
 }
