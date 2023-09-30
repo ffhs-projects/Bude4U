950b9332555c94bf5c9363e6ec45956c947e93c9
Achim Grether <a.grether@hotmail.com>
2023-09-30 23:56:11 +0200

    change to advertisement datamodel

--- src/main/java/ch/ffhs/bude4u/advertisement/Advertisement.java
+++ src/main/java/ch/ffhs/bude4u/advertisement/Advertisement.java
@@ -1,17 +1,21 @@
 package ch.ffhs.bude4u.advertisement;
 
+import lombok.Getter;
+
 import java.util.Dictionary;
 import java.util.List;
 
+@Getter
 public class Advertisement {
 
-    public Advertisement(String id, String title, String description, double price, double rooms, int space) {
+    public Advertisement(String id, String title, String description, double price, double rooms, int space, String mainPicUrl) {
         id = id;
         advertisementTitle = title;
         mainDescription = description;
         buyPrice = price;
         numberRooms = rooms;
         livingSpace = space;
+        mainPictureUrl = mainPicUrl;
     }
 
     String id;
@@ -19,6 +23,8 @@ public class Advertisement {
     private String advertisementTitle;
 
     private String mainDescription;
+
+    private String mainPictureUrl;
 
     private double buyPrice;
 
--- src/main/java/ch/ffhs/bude4u/advertisement/AdvertisementStub.java
+++ src/main/java/ch/ffhs/bude4u/advertisement/AdvertisementStub.java
@@ -49,11 +49,11 @@ public class AdvertisementStub implements GenericDAO<Advertisement> {
 
     private List<Advertisement> CreateMockData() {
         List<Advertisement> mockAdvertisements = new ArrayList<Advertisement>();
-        mockAdvertisements.add(new Advertisement("1", "Haus 1", "Schön hier 1...", 1001, 5.5, 2003));
-        mockAdvertisements.add(new Advertisement("2", "Haus 2", "Schön hier 2...", 1002, 4.5, 2003));
-        mockAdvertisements.add(new Advertisement("3", "Haus 3", "Schön hier 3...", 1003, 3.5, 2002));
-        mockAdvertisements.add(new Advertisement("4", "Haus 4", "Schön hier 4...", 1004, 2.5, 2001));
-        mockAdvertisements.add(new Advertisement("5", "Haus 5", "Schön hier 5...", 1005, 1.5, 2000));
+        mockAdvertisements.add(new Advertisement("1", "Haus 1", "Schön hier 1...", 1001, 5.5, 2003, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
+        mockAdvertisements.add(new Advertisement("2", "Haus 2", "Schön hier 2...", 1002, 4.5, 2003, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
+        mockAdvertisements.add(new Advertisement("3", "Haus 3", "Schön hier 3...", 1003, 3.5, 2002, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
+        mockAdvertisements.add(new Advertisement("4", "Haus 4", "Schön hier 4...", 1004, 2.5, 2001, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
+        mockAdvertisements.add(new Advertisement("5", "Haus 5", "Schön hier 5...", 1005, 1.5, 2000, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
         return mockAdvertisements;
     }
 }
