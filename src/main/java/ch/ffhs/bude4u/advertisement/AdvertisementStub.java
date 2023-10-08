02424f7c62fe56715048d47de131ea352ed98855
Patrick <patrick.morgenegg@students.ffhs.ch>
2023-10-09 00:03:36 +0200

    - Prettify home and advertisement views/layouts

--- src/main/java/ch/ffhs/bude4u/advertisement/AdvertisementStub.java
+++ src/main/java/ch/ffhs/bude4u/advertisement/AdvertisementStub.java
@@ -54,11 +54,11 @@ public class AdvertisementStub implements GenericDAO<Advertisement> {
 
     private List<Advertisement> CreateMockData() {
         List<Advertisement> mockAdvertisements = new ArrayList<Advertisement>();
-        mockAdvertisements.add(new Advertisement(UUID.fromString("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3454"), "Haus 1", "Schön hier 1...", "01.01.2023", "Haus", "offen", 1000, 5.5, 142, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
-        mockAdvertisements.add(new Advertisement(UUID.fromString("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3455"), "Haus 2", "Schön hier 2...", "01.01.2023", "Chalet", "offen", 2003, 5.5, 142, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
-        mockAdvertisements.add(new Advertisement(UUID.fromString("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3456"), "Haus 3", "Schön hier 3...", "01.01.2023", "Haus", "offen", 2002, 5.5, 142, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
-        mockAdvertisements.add(new Advertisement(UUID.fromString("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3457"), "Haus 4", "Schön hier 4...", "01.01.2023", "Wohnung", "offen", 2001, 5.5, 142, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
-        mockAdvertisements.add(new Advertisement(UUID.fromString("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3458"), "Haus 5", "Schön hier 5...", "01.01.2023", "Haus", "offen", 2000, 5.5, 142, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
+        mockAdvertisements.add(new Advertisement(UUID.fromString("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3454"), "Haus 1", "Schön hier 1...", "01.01.2023", "Haus", "offen", 100000, 5.5, 142, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
+        mockAdvertisements.add(new Advertisement(UUID.fromString("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3455"), "Haus 2", "Schön hier 2...", "01.02.2023", "Chalet", "reserviert", 240000, 5.5, 142, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
+        mockAdvertisements.add(new Advertisement(UUID.fromString("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3456"), "Haus 3", "Schön hier 3...", "01.03.2023", "Haus", "offen", 585000, 5.5, 142, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
+        mockAdvertisements.add(new Advertisement(UUID.fromString("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3457"), "Haus 4", "Schön hier 4...", "01.04.2023", "Wohnung", "offen", 1200000, 5.5, 142, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
+        mockAdvertisements.add(new Advertisement(UUID.fromString("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3458"), "Haus 5", "Schön hier 5...", "01.05.2023", "Haus", "reserviert", 30000, 5.5, 142, "https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_1280.jpg"));
         return mockAdvertisements;
     }
 }
